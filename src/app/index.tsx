aed4ed4911659154ecd49fa715a0ae4a7696d5c5
Richard Germaine <54249860+rlgerma92@users.noreply.github.com>
2022-07-10 12:01:54 -0600

    added create and read fs functions

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,79 +1,11 @@
-import {
-  createTheme,
-  Stack,
-  Text,
-  Link,
-  FontWeights,
-  IStackTokens,
-  IStackStyles,
-  ITextStyles,
-  ThemeProvider,
-} from "@fluentui/react";
+import { createTheme, ThemeProvider } from "@fluentui/react";
 
 import { theme } from "../config";
-
-const boldStyle: Partial<ITextStyles> = {
-  root: { fontWeight: FontWeights.semibold },
-};
-const stackTokens: IStackTokens = { childrenGap: 15 };
-
-const stackStyles: Partial<IStackStyles> = {
-  root: {
-    width: "960px",
-    margin: "0 auto",
-    textAlign: "center",
-    color: "#605e5c",
-  },
-};
 
 const ssTheme = createTheme(theme);
 
 function App() {
-  return (
-    <ThemeProvider applyTo="body" theme={ssTheme}>
-      <Stack
-        horizontalAlign="center"
-        verticalAlign="center"
-        verticalFill
-        styles={stackStyles}
-        tokens={stackTokens}
-      >
-        <Text variant="xxLarge" styles={boldStyle}>
-          Welcome to your Fluent UI app
-        </Text>
-
-        <Text variant="large">
-          For a guide on how to customize this project, check out the Fluent UI
-          documentation.
-        </Text>
-        <Text variant="large" styles={boldStyle}>
-          Essential links
-        </Text>
-        <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-          <Link href="https://developer.microsoft.com/en-us/fluentui#/get-started/web">
-            Docs
-          </Link>
-          <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">
-            Stack Overflow
-          </Link>
-          <Link href="https://github.com/microsoft/fluentui/">Github</Link>
-          <Link href="https://twitter.com/fluentui">Twitter</Link>
-        </Stack>
-        <Text variant="large" styles={boldStyle}>
-          Design system
-        </Text>
-        <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-          <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web/icons">
-            Icons
-          </Link>
-          <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web">
-            Styles
-          </Link>
-          <Link href="https://aka.ms/themedesigner">Theme designer</Link>
-        </Stack>
-      </Stack>
-    </ThemeProvider>
-  );
+  return <ThemeProvider applyTo="body" theme={ssTheme}></ThemeProvider>;
 }
 
 export default App;
